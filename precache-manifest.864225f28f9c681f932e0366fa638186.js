058e078a64b29b5709cf6a60ae6e5120ccfcce65
klas1998 <lutika.vlad@mail.ru>
2020-11-23 20:36:53 +0200

    Updates

---
+++ precache-manifest.864225f28f9c681f932e0366fa638186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1905de096424e2906be8065317983f12",
+    "url": "/TodoReact.deploy/index.html"
+  },
+  {
+    "revision": "fe5b4274168b5409e741",
+    "url": "/TodoReact.deploy/static/css/main.1b25c482.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/TodoReact.deploy/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoReact.deploy/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5b4274168b5409e741",
+    "url": "/TodoReact.deploy/static/js/main.40b573c6.chunk.js"
+  },
+  {
+    "revision": "93b3047faafa37d056f0",
+    "url": "/TodoReact.deploy/static/js/runtime-main.0841adf3.js"
+  }
+]);
